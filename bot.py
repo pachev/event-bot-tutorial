21167165e40a2fa68f06e5771c60e5e3c66bbf51
Pachev Joseph <pachevjoseph@gmail.com>
2018-02-16 21:08:07 -0800

    Adds event creation and sqlalchemy models

--- bot.py
+++ bot.py
@@ -1,6 +1,20 @@
 from os import getenv
 import discord
 from discord.ext import commands
+from sqlalchemy import engine, create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+from models import Base, Event, Member, Attendance
+
+engine = create_engine('sqlite:///event-bot.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# If table doesn't exist, Create the database
+if not engine.dialect.has_table(engine, 'event'):
+    Base.metadata.create_all(engine)
 
 description = 'A nice little event bot'
 bot = commands.Bot(command_prefix='?', description=description)
@@ -22,4 +36,29 @@ async def ping(ctx):
     server = ctx.message.server.name
     await bot.say('Pong for {} from {}!'.format(author, server))
 
-bot.run(token)
+@bot.command(pass_context=True)
+async def create(ctx, name: str, date: str, time: str='0:00am'):
+    '''Creates an event with specified name and date
+        example: ?create party 12/22/2017 1:40pm
+    '''
+    server = ctx.message.server.name
+    date_time = '{} {}'.format(date, time) 
+    try:
+        event_date = datetime.strptime(date_time, '%m/%d/%Y %I:%M%p')
+        event = Event(name=name, server=server, date=event_date)
+        session.add(event)
+        session.commit()
+        await bot.say('Event {} created successfully for {}'.format(name, event.date))
+    except Exception as e:
+        await bot.say('Could not complete your command')
+        print(e)
+
+if __name__ == '__main__':
+    try:
+        bot.run(token)
+    except Exception as e:
+        print('Could Not Start Bot')
+        print(e)
+    finally:
+        print('Closing Session')
+        session.close()
